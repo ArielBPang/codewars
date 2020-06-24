aaf1a2ccee3c6f7669e5b021ee90fc5bb5f14fad
Ariel Bo Pang <arielbpang@gmail.com>
2020-06-23 22:07:06 -0500

    first

---
+++ TankTruck.js
@@ -0,0 +1,30 @@
+//To introduce the problem think to my neighbor who drives a tanker truck. The level indicator is down and he is worried because he does not know if he will be able to make deliveries. We put the truck on a horizontal ground and measured the height of the liquid in the tank.
+
+//Fortunately the tank is a perfect cylinder and the vertical walls on each end are flat. The height of the remaining liquid is h, the diameter of the cylinder is d, the total volume is vt (h, d, vt are positive or null integers). You can assume that h <= d.
+
+//Could you calculate the remaining volume of the liquid? Your function tankvol(h, d, vt) returns an integer which is the truncated result (e.g floor) of your float calculation.
+
+//Examples:
+
+//tankvol(40,120,3500) //should return 1021 (calculation gives about: 1021.26992027)
+
+//tankvol(60,120,3500) //should return 1750
+
+tankvol(80,120,3500) //should return 2478 (calculation gives about: 2478.73
+
+function tankvol(h, d, vt) {
+  var radius = d/2;
+  var volume = 0;
+  if(h==radius){
+    return vt/2;
+  }
+    var angle = Math.acos((radius-h)/radius);
+    var slice_volume = ((angle*2)/(Math.PI*2))*vt;
+    var b = Math.sin(angle)*radius;
+    var triangle_area = b*(radius-h);
+    var circle_area = Math.PI*(radius**2);
+    var lengthTank = vt / circle_area;
+    var triangle_volume = triangle_area * lengthTank;
+    var volume = slice_volume - triangle_volume;
+    return Math.floor(volume);
+}
---
+++ highestScoringWord.js
@@ -0,0 +1,50 @@
+//Given a string of words, you need to find the highest scoring word.
+
+//Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+//You need to return the highest scoring word as a string.
+
+//If two words score the same, return the word that appears earliest in the original string.
+
+//All letters will be lowercase and all inputs will be valid.
+
+
+function high(x){
+    var wordList = x.split(" ");
+    console.log("wordList: " + wordList);
+    var getScore = (word) => {
+      return word.split('').reduce((prevScore, currWord) => prevScore + currWord.charCodeAt(0) - 96, 0)
+    }
+    var scoreList = wordList.map(word => getScore(word));
+  
+    // 3. get the highest score and index
+    let highestIndex = 0;
+    let highestScore = 0;
+    scoreList.forEach((score, i) => {
+      if (score > highestScore) {
+        highestIndex = i;
+        highestScore = score;
+      }
+    });
+    
+    // 4. return the string of the highest score index of wordList
+    return wordList[highestIndex];
+  }
+  
+  function high(x){
+    //transform the input string into array & define a string of alphabetical latin characters
+    var arr = x.split(' ');
+    var str = 'abcdefghijklmnopqrstuvwxyz';
+    //Iterate through the array with input words to find the one with the greatest sum
+    var newArr = arr.map(function(word){
+      var sum = 0;
+      for (var i = 0; i < word.length; i++) {
+        sum += str.indexOf(word[i]);
+      }
+      return sum;
+    });
+    //Return the word with the greatest sum
+    return arr[newArr.indexOf(Math.max(...newArr))];
+  }
+  
+  high("hey there friend");
